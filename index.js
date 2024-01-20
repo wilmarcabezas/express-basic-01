870df7905cdee9d76806b005a7f989fa27565719
AntonioHcs <antonio.skate.1709@gmail.com>
2024-01-20 12:56:14 -0600

    Metodo PATCH terminado

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ app.use(express.json());
 
 const port = 4000; 
 const data = [
-  { id: 40, name: "Claudia" },
-  { id: 20, name: "Andrea" },
-  { id: 30, name: "Antonio" },
-  { id: 10, name: "Baltazar" },
+  { id: 40, name: "Claudia",age:20 },
+  { id: 20, name: "Andrea",age:18 },
+  { id: 30, name: "Antonio",age:21 },
+  { id: 10, name: "Baltazar",age:22 },
 ];
 
 // PRINCIPIOS SOLID
@@ -44,6 +44,23 @@ app.delete('/student/:id', (req, res) => {
   }
   console.log(data);
   return res.status(200).send({ data: 'El Id ' + req.params.id + ' de Estudiante no ha sido encontrado' });
+})
+
+app.patch('/student/:id',(req,res) => {
+  const id = parseInt(req.params.id);
+  const {name} = req.body;
+  const {age} = req.body;
+  
+  const student = data.find(item => item.id === id);
+  if(student){
+    student.name = name;
+    student.age = age;
+    return res.send({message:'Proceso realizado',data:student})
+  }
+  else{
+    return res.status(404).send({ data: 'El Id ' + req.params.id + ' de Estudiante no ha sido encontrado' });
+  }
+  
 })
 
 app.listen(port, () => {

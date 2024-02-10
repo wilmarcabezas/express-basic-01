19eb921f3cc0aa2eb2679f193c90418bd9fba241
baltazarnavado <baltazar_n@yahoo.com>
2024-02-10 10:28:46 -0800

    Se actualizaron las rutas Get (con y sin parametro) para el manejo correcto de excepciones

--- controllers/teachersController.js
+++ controllers/teachersController.js
@@ -3,10 +3,6 @@ const router = express.Router();
 
 /**
  * @swagger
- * openapi: 3.0.0
- * info:
- *   title: API de Gestión de Estudiantes
- *   version: "1.0"
  * paths:
  *   /teachers:
  *     get:
@@ -22,16 +18,47 @@ const router = express.Router();
  *               schema:
  *                 type: object
  *                 properties:
- *                   message:
+ *                   id: 
+ *                    type: integer
+ *                    description: El Id del Profesor en Cuestion.
+ *                    example: 1
+ *                   name:
  *                    type: string
+ *                    description: El Nombre del Profesor
+ *                    example: Pierre Huguet
+ *         500:
+ *           description: Existe un problema con el servidor.
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   error: 
+ *                    type: string
+ *                    example: No fue posible obtener la lista de los profesores.     
+ * 
  */
+const dataProfesores = [{id: 1, name: 'Pierre Huguet'}, {id:2, name:'Scott Mitchel'}]
 
 router.get('/',(req,res)=>{
-    res.json({message: "Lista de Profesores"});
+    try {
+        res.status(200).send(dataProfesores);
+    } catch (error) {
+        res.status(500).send({error: error});
+    }
 })
 
 router.get('/:id',(req,res)=>{
-    res.json({message: "Profesor con id: " + req.params.id});
+    try {
+        const idProfesor = req.params.id;
+        const resultado = dataProfesores.filter(profesor => profesor.id == idProfesor);
+        if (resultado.length <= 0){
+            return res.status(404).send({error: 'Profesor no encontrado'})
+        }
+        return res.status(200).send(resultado);
+    } catch (error) {
+        res.status(500).send({error: error.message})
+    }
 })
 
 
